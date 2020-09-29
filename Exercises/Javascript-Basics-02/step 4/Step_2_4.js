cf3e81080249134e3c447ea741211d76faa4dc9d
Ali-Traboulsi <alitraboulsi112.gmail.com>
2020-09-29 13:48:25 +0300

    Completed-and-edtited-JS2-Step-4

--- Exercises/Javascript-Basics-02/step 4/Step_2_4.js
+++ Exercises/Javascript-Basics-02/step 4/Step_2_4.js
@@ -4,9 +4,11 @@ var resetCity = document.querySelector("#city");
 var resetButton = document.querySelector("button");
 
 resetButton.addEventListener("click", function(){
-     resetName.value = "";
-     resetSurname.value = "";
-     resetCity.value = "";
-
+    var answer = window.confirm("Do You really want to proceed? \nEverything will be deleted");
+    if(answer == true){
+        resetName.value = "";
+        resetSurname.value = "";
+        resetCity.value = "";
+    }
 });
 
